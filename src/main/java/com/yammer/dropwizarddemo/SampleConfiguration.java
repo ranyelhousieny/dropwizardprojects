6658c4cf7bf164ef121743c469d144019059fc10
Rany <ranyel@msn.com>
2020-03-13 21:28:03 -0700

    Working project

--- src/main/java/com/yammer/dropwizarddemo/SampleConfiguration.java
+++ src/main/java/com/yammer/dropwizarddemo/SampleConfiguration.java
@@ -1,20 +1,8 @@
 package com.yammer.dropwizarddemo;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.yammer.dropwizard.config.Configuration;
 
-import javax.validation.constraints.NotEmpty;
 
 public class SampleConfiguration extends Configuration {
-    @NotEmpty
-    private String password;
 
-    @JsonProperty
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/main/java/com/yammer/dropwizarddemo/SampleService.java
+++ src/main/java/com/yammer/dropwizarddemo/SampleService.java
@@ -3,11 +3,7 @@ package com.yammer.dropwizarddemo;
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
-import com.yammer.dropwizarddemo.com.yammer.dropwizarddemo.dto.Car;
 import com.yammer.dropwizarddemo.resources.SampleResource;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class SampleService extends Service<SampleConfiguration> {
 
